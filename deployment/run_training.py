384bcc8fd5b9c5f36686fc88d399f8769586083d
shubhranshumishra1998-hub <shubhranshumishra1998@gmail.com>
2026-02-22 08:41:18 +0530

    Update run_training.py

--- deployment/run_training.py
+++ deployment/run_training.py
@@ -1,1 +1,18 @@
+import sagemaker
+from sagemaker.sklearn.estimator import SKLearn
 
+role = sagemaker.get_execution_role()
+session = sagemaker.Session()
+
+estimator = SKLearn(
+    entry_point="src/training/train.py",
+    role=role,
+    instance_type="ml.m5.large",
+    framework_version="1.2-1",
+    py_version="py3",
+    hyperparameters={}
+)
+
+estimator.fit({
+    "train": "s3://bucket2-curated-stock/ml/train/"
+})
